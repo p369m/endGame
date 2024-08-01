becffe7564abf86eb6b68b4f20718b5fd06cca09
Partha Mandal <parthamandal621@gmail.com>
2024-08-01 17:16:47 +0530

    first commit

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  console.log(`${process.env.MONGO_DB_URL}/${DB_NAME}`);
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_DB_URL}/${DB_NAME}`
+    );
+    console.log("MongoDB Connected ! ", connectionInstance);
+  } catch (error) {
+    console.log("MongoDB Connection ERROR", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+//import express from "express";
+//const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGO_DB_URL}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERROR", error);
+//       throw error;
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server listening on PORT ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("ERROR", error);
+//     throw error;
+//   }
+// })();
