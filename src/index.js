9b3e760992b8ff56c4cd319f46b15ea876f75852
Partha Mandal <parthamandal621@gmail.com>
2024-08-02 11:22:08 +0530

    user and video model

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
-// require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
-//import express from "express";
-//const app = express();
-
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGO_DB_URL}/${DB_NAME}`);
-//     app.on("error", (error) => {
-//       console.log("ERROR", error);
-//       throw error;
-//     });
-//     app.listen(process.env.PORT, () => {
-//       console.log(`Server listening on PORT ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.log("ERROR", error);
-//     throw error;
-//   }
-// })();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Server Error ", err);
+    });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Server Listening ");
+    });
+  })
+  .catch((err) => {
+    console.log("DB Connection Error ", err);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,6 @@
+const asyncHandler = (requestHandler) => {
+  async (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+export { asyncHandler };
